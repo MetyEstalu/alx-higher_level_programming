15d8f32ef3cd9c85403d670c7f3554729fc674e0
MetyEstalu <Metyswety@yahoo.com>
2023-03-07 05:20:39 -0800

    task100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-i = 122
-while i >= 97:
-    flag = 0
-    if i % 2 != 0:
-        i = i - 32
-        flag = 1
-    print("{:s}".format(chr(i)), end="")
-    if flag == 1:
-        i = i + 32
-    i = i - 1
+
+for x in range(122, 96, -1):
+    if x % 2 != 0:
+        print("{}".format(chr(x - 32)), end="")
+    else:
+        print("{}".format(chr(x)), end="")
