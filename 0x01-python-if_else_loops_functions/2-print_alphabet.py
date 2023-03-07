a3b8b83f56576ca5bc2017fd285c6704f8f92bc9
MetyEstalu <Metyswety@yahoo.com>
2023-03-07 05:07:28 -0800

    task2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    print("{:s}".format(chr(i)), end="")
+
+for x in range(ord("a"), ord("z") + 1):
+    print("{}".format(chr(x)), end="")
 
